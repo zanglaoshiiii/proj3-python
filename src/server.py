ddbc087fa475b626b98e977d19029654315aceda
zanglaoshiiii <38207461+zanglaoshiiii@users.noreply.github.com>
2019-12-09 11:11:33 -0800

    Add files via upload

--- src/server.py
+++ src/server.py
@@ -1,7 +1,7 @@
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
 from socketserver import ThreadingMixIn
-from MyTimer import MyTimer
+
 import argparse
 import hashlib
 import xmlrpc.client
@@ -9,8 +9,8 @@ import random
 import time
 import threading
 
-
-fileinfomap = {}
+BlockStore = {}
+FileInfoMap = {}
 
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
@@ -18,70 +18,109 @@ class RequestHandler(SimpleXMLRPCRequestHandler):
 class threadedXMLRPCServer(ThreadingMixIn, SimpleXMLRPCServer):
     pass
 
+class timerClass():
+    '''Timer'''
+    def __init__(self):
+        self.t_a = 300
+        self.t_b = 500
+        self.start = int(time.time()*1000)
+        self.timeout = random.randint(self.t_a,self.t_b)
+
+    def now(self):
+        return int(time.time()*1000) - self.start
+
+    def reset(self):
+        self.start = int(time.time()*1000)
+        self.timeout = random.randint(self.t_a,self.t_b)
+
+    def setTimeout(self, reset_time = None):
+        if reset_time != None:
+            self.timeout = reset_time
+        else:
+            self.timeout = random.randint(self.t_a,self.t_b)
+
+
+
 # A simple ping, returns true
 def ping():
     """A simple ping method"""
-    print("Ping()")
+    #print("Ping()")
     return True
 
 # Gets a block, given a specific hash value
 def getblock(h):
     """Gets a block"""
-    print("GetBlock(" + h + ")")
+    # print("GetBlock(" + h + ")")
 
-    blockData = bytes(4)
+    #blockData = bytes(4)
+    blockData = BlockStore[h]
+    #print(BlockStore)
     return blockData
 
 # Puts a block
 def putblock(b):
     """Puts a block"""
-    print("PutBlock()")
+    # print("PutBlock()", b)
 
+    h = hashlib.sha256(b.data).hexdigest()
+    # print("index: ", h)
+    BlockStore[h] = b.data
     return True
 
 # Given a list of hashes, return the subset that are on this server
 def hasblocks(hashlist):
     """Determines which blocks are on this server"""
-    print("HasBlocks()")
+    # print("HasBlocks()")
 
-    return hashlist
+    haslist = []
+    haslist = [hashes for hashes in hashlist if hashes in BlockStore.keys()]
 
-# Retrieves the server's fileinfomap
+    return haslist
+
+# Retrieves the server's FileInfoMap
 def getfileinfomap():
     """Gets the fileinfo map"""
-    # print("Getfileinfomap()")
+    # print("GetFileInfoMap()")
+    
 
-    return fileinfomap
+    result = FileInfoMap
+    return result
 
 # Update a file's fileinfo entry
 def updatefile(filename, version, hashlist):
     """Updates a file's fileinfo entry"""
-    if isCrash or status != 0:
-        raise Exception('exception')
+    if is_crashed:
+        raise Exception('Crashed')
+    if state!=0:
+        raise Exception('not Leader')
     global log
+    # ******* add log entries
+    # log.append([current_term, ]) # check with others for their commits
+    last_index = len(log)
+    log.append([current_term, [filename, version, hashlist]])
+    #print(log)
 
-    log.append([currentTerm, [filename, version, hashlist]])
-    lastIndex = len(log) - 1
+    # ********block if majority down****
 
-    # block
-    while commitIndex < lastIndex:
-        continue
+    # wait until committed?
+    time.sleep(2)
+    if commit_index>=last_index:
+        print("file Updated")
+        return True
+    print("file not updated") 
+    return False
 
-    return True
-
-
-def apply(index):
-    filename, version, hashlist = log[index][1]
-    if filename in fileinfomap.keys():
-        latest_version = fileinfomap[filename][0]
-        if (version == latest_version + 1):
-            fileinfomap[filename] = [version, hashlist]
-            return True
+def apply(log_index):
+    filename, version, hashlist = log[log_index][1]
+    if filename in FileInfoMap.keys():
+        last_version = FileInfoMap[filename]
+        if (version == last_version[0]+1):
+            FileInfoMap[filename] = tuple((version, hashlist))
         else:
             return False
     else:
-        fileinfomap[filename] = [version, hashlist]
-        return True
+        FileInfoMap[filename] = tuple((version, hashlist))
+    return True
 
 # PROJECT 3 APIs below
 
@@ -90,10 +129,9 @@ def apply(index):
 def isLeader():
     """Is this metadata store a leader?"""
     print("IsLeader()")
-    if status == 0:
+    if state == 0:
         return True
-    else:
-        return False
+    return False
 
 # "Crashes" this metadata store
 # Until Restore() is called, the server should reply to all RPCs
@@ -101,19 +139,18 @@ def isLeader():
 # RPCs to other servers
 def crash():
     """Crashes this metadata store"""
-    global isCrash
+    global is_crashed
     print("Crash()")
-    isCrash = True
+    is_crashed = True
     return True
 
 # "Restores" this metadata store, allowing it to start responding
 # to and sending RPCs to other nodes
 def restore():
     """Restores this metadata store"""
-    global isCrash
+    global is_crashed
     print("Restore()")
-    isCrash = False
-    timer.reset()
+    is_crashed = False
     return True
 
 
@@ -122,94 +159,85 @@ def restore():
 def isCrashed():
     """Returns whether this node is crashed or not"""
     print("IsCrashed()")
-    return isCrash
+    return is_crashed
 
-def tester_getversion(filename):
+def getVersion(filename):
     "gets version number of file from server"
-    
-    return fileinfomap[filename][0]
+    if filename not in FileInfoMap.keys():
+        return 0
+    return FileInfoMap[filename][0]
     
 
 def requestVote(cl):
-    if isCrash:
+    global vote_counter
+    global current_term
+    global state
+
+    if is_crashed:
         raise Exception('Crashed')
     
-    global vote_counter
-    global currentTerm
-    global status
-
-
-    
     last_log_index = len(log) - 1
-    last_log_term = log[last_log_index][0]
+    last_log_term = log[-1][0]
+    # print("current_term",current_term)
     try:
-        vote_response = cl.voteHandler(currentTerm, servernum, last_log_index, last_log_term )
-
+        vote_response = cl.voteHandler(current_term, idx, last_log_index, last_log_term )
+        # print("vote_response",vote_response)
+        # print("vote requested from: ", cl)
         if vote_response[0]: # [true, current term]
             vote_counter +=1
         else:
-            if vote_response[1]>currentTerm:
-                currentTerm = vote_response[1]
-                status = 2
+            if vote_response[1]>current_term:
+                current_term = vote_response[1]
+                state = 2
             pass
             
-    except Exception:
+    except Exception as e:
+        #print(e)
         pass
 
 def voteHandler(cand_term, cand_id, cand_last_log_index, cand_last_log_term):
-    if isCrash:
+    if is_crashed:
         raise Exception('Crashed')
-
     global timer
-    global voteFor
-    global currentTerm
-    global status
-
     timer.reset()
 
     def castVote():
-        global voteFor
-        global currentTerm
-        global status
-        voteFor = cand_id
-        currentTerm = cand_term
-        status = 2
+        global voted_for
+        global current_term
+        global state
+        voted_for = cand_id
+        current_term = cand_term
+        state = 2
         print("casting vote to", cand_id)
-        return [True, currentTerm]
+        return [True, current_term]
 
-    if cand_term <= currentTerm:
-        return [False, currentTerm]
+    if cand_term <= current_term:
+        return [False, current_term]
     else:
         last_log_index = len(log)-1
         last_log_term = log[-1][0]
         print("last_log_term", last_log_term)
 
         if cand_last_log_index > last_log_index:
-            voteFor = cand_id
-            currentTerm = cand_term
-            status = 2
-            return [True, currentTerm]
+            return castVote()
         elif cand_last_log_index == last_log_index \
                 and cand_last_log_term == last_log_term:
-            voteFor = cand_id
-            currentTerm = cand_term
-            status = 2
-            return [True, currentTerm]
+            return castVote()
         else:
-            return [False, currentTerm]    
+            return [False, current_term]    
 
 
 def appendEntries(cl):
     global next_index
-    global matchIndex
+    global match_index
     global prev_log_index
-    global status
-    global currentTerm
+    global state
+    global current_term
     global success
     global prev_log_term
 
     try:
-        if newLeader:
+        if new_leader:
             next_index[cl] = len(log)
         prev_log_index[cl] = next_index[cl]-1
         prev_log_term[cl] = log[prev_log_index[cl]][0]
@@ -223,18 +251,18 @@ def appendEntries(cl):
             entries =[]
             #print("try again, next index: ",next_index[cl])
 
-        leader_commit = commitIndex
-        follower_term, success[cl] = cl.appendEntryHandler(currentTerm, servernum, prev_log_index[cl],\
+        leader_commit = commit_index
+        follower_term, success[cl] = cl.appendEntryHandler(current_term, idx, prev_log_index[cl],\
                                 prev_log_term[cl], entries, leader_commit)
         #success True if follower[next_index] matches any entry in leader or it has just been appended
 
-        if follower_term > currentTerm:
-            status = 2
-            currentTerm = follower_term
+        if follower_term > current_term:
+            state = 2
+            current_term = follower_term
 
         if success[cl]:
             if len(entries)>0: prev_log_index[cl] += len(entries)-1
-            matchIndex[cl] = prev_log_index[cl]
+            match_index[cl] = prev_log_index[cl]
             next_index[cl] = prev_log_index[cl]+1
         else:
             if next_index[cl] > 0:
@@ -247,16 +275,16 @@ def appendEntries(cl):
 def appendEntryHandler(leader_term, leader_id, prev_log_index,\
                         prev_log_term, entries, leader_commit):
     #print("log",log)
-    if isCrash:
+    if is_crashed:
         raise Exception('Crashed')
 
     global timer
-    global currentTerm
-    global status
-    global commitIndex
+    global current_term
+    global state
+    global commit_index
 
-    if leader_term < currentTerm:
-        return currentTerm, False
+    if leader_term < current_term:
+        return current_term, False
 
     def appendLog():
         print("in appendLog")
@@ -267,166 +295,143 @@ def appendEntryHandler(leader_term, leader_id, prev_log_index,\
             else:
                 log.append(entries[i])
 
-    status = 2  #*** maybe
-    currentTerm = leader_term
+    state = 2  #*** maybe
+    current_term = leader_term
     timer.reset()
-    print("hearbeat from "+ str(leader_id)+" in term: "+str(currentTerm))
+    print("hearbeat from "+ str(leader_id)+" in term: "+str(current_term))
     print("received entries:", entries)
 
     if len(log)-1< prev_log_index or log[prev_log_index][0] != prev_log_term:  #*** maybe
-        return currentTerm, False
+        return current_term, False
 
     if entries != []:
         appendLog()
 
-    if leader_commit > commitIndex:
-        commitIndex = min(leader_commit, len(log)-1)
+    if leader_commit > commit_index:
+        commit_index = min(leader_commit, len(log)-1)
 
         
-    return currentTerm, True 
+    return current_term, True 
 
 def raftHandler():
-    global currentTerm
-    global status
+    global current_term
+    global state
     global vote_counter
     global timer
-    global newLeader
+    global new_leader
     global next_index
-    global matchIndex
+    global match_index
     global success
     global prev_log_index
     global prev_log_term
-    global commitIndex
+    global commit_index
     global last_applied
 
-    timer = MyTimer()
+    timer = timerClass()
     timer.reset()
     while True:
-        while isCrash:
-            status = 2
-            pass
-        if commitIndex > last_applied:
+        if is_crashed:
+            state = 2
+            continue
+        if commit_index > last_applied:
             if apply(last_applied+1):
                 last_applied += 1
-        if status !=0:
-            if timer.currentTime() > timer.timeout:
-                status = 1  # candidate
-                currentTerm +=1
+        if state !=0:
+            if timer.now() > timer.timeout:
+                state = 1  # candidate
+                current_term +=1
                 vote_counter = 0 #initialized
                 vote_counter += 1 # vote for self
                 timer.reset()
                 th11_list = []
-                for cl in serverList:
+                for cl in client_list:
                     th11_list.append(threading.Thread(target = requestVote, args=(cl, )))
                     th11_list[-1].start()
                 for t in th11_list:
                     t.join()
                 #print(vote_counter)
-                if vote_counter > (numServers/2):
-                    status = 0 #leader elected
-                    newLeader = True
-                    print("I am the leader in term: " + str(currentTerm) +", votes: " + str(vote_counter))
+                if vote_counter > (num_servers/2):
+                    state = 0 #leader elected
+                    new_leader = True
+                    print("I am the leader in term: " + str(current_term) +", votes: " + str(vote_counter))
                     print(log)
                     # immediately send hearbeat here somehow
         else: # leader
             timer.setTimeout(100)
-            if timer.currentTime() > timer.timeout:
+            if timer.now() > timer.timeout:
                 timer.reset()
                 th12_list= []
-                if newLeader:
+                if new_leader:
                     next_index ={}
-                    matchIndex = {}
+                    match_index = {}
                     success={}
                     prev_log_index = {}
                     prev_log_term = {}
-                    for cl in serverList:
+                    for cl in client_list:
                         next_index[cl] = len(log) # [initialize]
-                        matchIndex[cl] = 0
+                        match_index[cl] = 0
                         success[cl] = False
 
 
-                for cl in serverList:
+                for cl in client_list:
                     th12_list.append(threading.Thread(target = appendEntries, args=(cl, )))
                     th12_list[-1].start()
                 for t in th12_list:
                     t.join()
-                #commitIndex = min([matchIndex[cl] for cl in serverList]) #*** to be implemented
-                newLeader = False
+                #commit_index = min([match_index[cl] for cl in client_list]) #*** to be implemented
+                new_leader = False
 
-                if len(log)-1 > commitIndex:
+                if len(log)-1 > commit_index:
                     commit_count = 1
-                    for cl in serverList:
-                        if cl in matchIndex.keys() and matchIndex[cl]>commitIndex:
+                    for cl in client_list:
+                        if cl in match_index.keys() and match_index[cl]>commit_index:
                             commit_count += 1
-                    if commit_count > (numServers/2) and log[commitIndex+1][0]==currentTerm:
-                        commitIndex += 1
-                    print("Leader commit index: ", commitIndex)
-
-# Reads the config file and return host, port and store list of other servers
-def readconfig(config, servernum):
-    """Reads cofig file"""
-    fd = open(config, 'r')
-    l = fd.readline()
-
-    maxnum = int(l.strip().split(' ')[1])
-
-    if servernum >= maxnum or servernum < 0:
-        raise Exception('Server number out of range.')
-
-    d = fd.read()
-    d = d.splitlines()
-
-    for i in range(len(d)):
-        hostport = d[i].strip().split(' ')[1]
-        if i == servernum:
-            host = hostport.split(':')[0]
-            port = int(hostport.split(':')[1])
-
-        else:
-            serverlist.append(hostport)
-
-
-    return maxnum, host, port
+                    if commit_count > (num_servers/2) and log[commit_index+1][0]==current_term:
+                        commit_index += 1
+                    print("Leader commit index: ", commit_index)
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="SurfStore server")
-    # parser.add_argument('config_file', help='path to config file')
-    # parser.add_argument('servernum', help='server id')
-    parser.add_argument('config', help='path to config file')
-    parser.add_argument('servernum', type=int, help='server number')
-
+    parser.add_argument('config_file', help='path to config file')
+    parser.add_argument('idx', help='server id')
     args = parser.parse_args()
 
-    config = args.config
-    servernum = args.servernum
-    
-    # server list has list of other servers
-    serverlist = []
-    
-    # maxnum is maximum number of servers
-    maxnum, host, port = readconfig(config, servernum)
+    config_file = args.config_file
+    idx = int(args.idx)
 
-    serverList = []
-    for s in serverlist:
-        if s != servernum:
-            serverList.append(xmlrpc.client.ServerProxy("http://" + s))
+    server_info = {}
 
+    with open(config_file,'r') as file:
+        next(file)
+        for line in file:
+            server_info[int(line.split(' ')[0][-2])] = line.strip().split(' ')[1]
 
-    numServers = len(serverList) + 1
-    status = 2   # 0: Leader; 1: Candidate; 2: Follower
-    isCrash = False
-    currentTerm = 1
-    voteFor = None
-    log = [] # [[term,data]]
-    newLeader = False
-    commitIndex = 0
+    address, port = server_info[idx].split(':')
+    port = int(port)
+    print(port)
+
+    num_servers = len(server_info)
+    state = 2   # 0: Leader; 1: Candidate; 2: Follower
+    is_crashed = False
+    current_term = 1
+    voted_for = None
+    log = [[0,0]] # [[term,data]]
+    new_leader = False
+    commit_index = 0
     last_applied = 0
-    matchIndex = {}
+    match_index = {}
 
-    server = threadedXMLRPCServer((host, port), requestHandler=RequestHandler)
-
+    print("Attempting to start XML-RPC Server at "+ address+":"+str(port))
+    server = threadedXMLRPCServer((address, port), requestHandler=RequestHandler)
+    # th1 = threading.Thread(target = raftThread)
+    
+    client_list = []
+    for i in server_info.keys():
+        if i!=idx:
+            cl = xmlrpc.client.ServerProxy("http://"+server_info[i])
+            client_list.append(cl)
 
     server.register_introspection_functions()
     server.register_function(ping,"surfstore.ping")
@@ -440,7 +445,7 @@ if __name__ == "__main__":
     server.register_function(crash,"surfstore.crash")
     server.register_function(restore,"surfstore.restore")
     server.register_function(isCrashed,"surfstore.isCrashed")
-    server.register_function(tester_getversion, "surfstore.tester_getversion")
+    server.register_function(getVersion, "surfstore.tester_getversion")
 
     server.register_function(voteHandler,"voteHandler")
     server.register_function(appendEntryHandler, "appendEntryHandler")
